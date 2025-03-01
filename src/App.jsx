b4804c7f9850f10af8b4fa30a34d2112a4c66ba5
wen2811 <wenbehr@gmail.com>
2025-03-01 17:40:04 +0100

    created context hook to check the authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import './App.css'
 import {Login} from "./pages/login/login.jsx";
+import {useAuth} from "./context/authContext.jsx";
+
+// Protected route component that checks auth status
+const ProtectedRoute = ({ children }) => {
+    const { user } = useAuth();
+
+    if (!user) {
+        // Redirect to login if not authenticated
+        return <Navigate to="/login" />;
+    }
+
+    return children;
+};
+
+
 
 function App() {
+    const {user} = useAuth();
 
-    return (<BrowserRouter>
+    return (
+
             <Routes>
-                <Route path="/" element={<Login />}/>
+                <Route path="/" element={<ProtectedRoute><h1>Dashboard</h1></ProtectedRoute>}/>
                 <Route path="/login" element={<Login />} />
-
-
             </Routes>
-        </BrowserRouter>
-
-
     )
 }
 
