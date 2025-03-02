b13a83e1a72cd058dc82b5d273c6d3988a06e95b
wen2811 <wenbehr@gmail.com>
2025-03-02 15:22:09 +0100

    add login page with authentication functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import './App.css'
-import {Login} from "./pages/login/login.jsx";
+import Login from "./pages/login/login.jsx";
 import {useAuth} from "./context/authContext.jsx";
 
 // Protected route component that checks auth status
