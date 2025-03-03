f3af55ed419538d9caec1e145070f45e84a76bb4
wen2811 <wenbehr@gmail.com>
2025-03-03 18:28:42 +0100

    created card component

---
+++ src/components/UI/card/Card.jsx
@@ -0,0 +1,20 @@
+
+export const Card = ({
+                         children,
+                         className = '',
+                         ...props
+                     }) => {
+    const cardClasses = [
+        'card',
+        className
+    ].filter(Boolean).join(' ');
+
+    return (
+        <div
+            className={cardClasses}
+            {...props}
+        >
+            {children}
+        </div>
+    );
+};
