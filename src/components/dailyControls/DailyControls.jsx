10ff0cb373ee5da9a45d5de05a8873e287ab4a59
wen2811 <wenbehr@gmail.com>
2025-03-31 17:31:19 +0200

    Added DailyControls component for user actions

--- src/components/dailyControls/DailyControls.jsx
+++ src/components/dailyControls/DailyControls.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BookmarkIcon, PlusCircle} from "lucide-react";
 import {Input} from "../UI/input/Input.jsx";
 import {Button} from "../UI/button/Button.jsx";
+import './DailyControls.css';
 
 export const DailyControls = ({ selectedDate, onDateChange, onAddMeal, onAddFood }) => {
     return (
