e2efd8f422aee4586a4989227f7fe9624d6cc56c
wen2811 <wenbehr@gmail.com>
2025-03-31 17:30:39 +0200

    Added Dashboard page structure and placeholder

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import './Dashboard.css'
+import {DailyControls} from "../../components/dailyControls/DailyControls.jsx";
+import {MacroSummary} from "../../components/macroSummary/MacroSummary.jsx";
+
 
 const Dashboard = () => {
+    const dailyMacros = {
+        totalCalories: 1800,
+        totalProtein: 120,
+        totalCarbs: 200,
+        totalFat: 70
+    };
+
     return (
         <div className="dashboard-page">
             <h1>Dashboard</h1>
             <p>Welkom terug! Hier zie je jouw dagelijkse voeding en macro’s.</p>
+
+            <DailyControls
+                selectedDate={new Date().toISOString().split("T")[0]}
+                onAddClick={() => console.log("Maaltijd toevoegen")}
+                onDateChange={(date) => console.log("Gekozen datum:", date)}
+            />
+
+            <MacroSummary dailyMacros={dailyMacros} />
         </div>
     );
 };
